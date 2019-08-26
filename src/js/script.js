7695293387c2dde33442a22bb24df4fb3f040112
Jakub Firlej <jakub.firlej@outlook.com>
2019-08-26 21:20:31 +0200

    create initCart

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -26,11 +27,33 @@
     },
     widgets: {
       amount: {
+        input: 'input.amount', // CODE CHANGED
         input: 'input[name="amount"]',
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
+
   };
 
   const classNames = {
@@ -38,6 +61,12 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active',
+    },
+    // CODE ADDED END
+
   };
 
   const settings = {
@@ -45,11 +74,21 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+
+    },
+    cart: {
+      defaultDeliveryFee: 20,
+    },
+    // CODE ADDED END
   };
+
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
+
   };
   class Product {
     constructor(id, data) {
@@ -219,8 +258,8 @@
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions();
 
-      console.log('AmountWidget:', thisWidget);
-      console.log('constructor arguments:', element);
+      // console.log('AmountWidget:', thisWidget);
+      // console.log('constructor arguments:', element);
     }
     getElements(element) {
       const thisWidget = this;
@@ -262,15 +301,15 @@
         event.preventDefault();
         thisWidget.setValue(thisWidget.value - 1);
 
-        console.log(thisWidget.value, 'wywyołanie eventu odejmij  ');
+        //console.log(thisWidget.value, 'wywyołanie eventu odejmij  ');
 
 
       });
       thisWidget.linkIncrease.addEventListener('click', function () {
         event.preventDefault();
-        thisWidget.setValue(thisWidget.value + 1)
+        thisWidget.setValue(thisWidget.value + 1);
 
-        console.log(thisWidget.value, 'wywyołanie eventu dodaj  ');
+        // console.log(thisWidget.value, 'wywyołanie eventu dodaj  ');
       });
     }
     announce() {
@@ -279,12 +318,25 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart', thisCart);
+    }
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+    }
+  }
 
 
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
+      // console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
@@ -299,11 +351,11 @@
 
     init: function () {
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      //console.log('*** App starting ***');
+      // console.log('thisApp:', thisApp);
+      //  console.log('classNames:', classNames);
+      //  console.log('settings:', settings);
+      //  console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
